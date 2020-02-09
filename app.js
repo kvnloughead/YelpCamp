4974837353a5bd5adc8c458de62e2879f62f8c6c
Kevin Loughead <kvnloughead@gmail.com>
2020-02-09 11:41:07 -0500

    Added create new campground functionality.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const request = require("request");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -10,13 +12,32 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+var campgrounds = [
+    {name: "Camp Yogi", image: "https://images.unsplash.com/photo-1532720401185-3c5adeba363c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Hooverville", image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Never-Never Land", image: "https://images.unsplash.com/photo-1546890975-7596e98cdbf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+]
+
+// main campgrounds page
 app.get("/campgrounds", function(req, res){
-    let campgrounds = [
-        {name: "Camp Yogi", image: "https://images.unsplash.com/photo-1532720401185-3c5adeba363c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-        {name: "Hooverville", image: "https://images.unsplash.com/photo-1533575770077-052fa2c609fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-        {name: "Never-Never Land", image: "https://images.unsplash.com/photo-1546890975-7596e98cdbf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-    ]
     res.render("campgrounds", {campgrounds: campgrounds})
+});
+
+// POST new campground
+app.post("/campgrounds", function(req, res) {
+    // add new campground to campgrounds array
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect to campgrounds (used GET by default)
+    res.redirect("/campgrounds");
+
+});
+
+// form for new campground
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
 });
 
 app.listen(3000, function(){
--- commit-changes.md
+++ commit-changes.md
@@ -1,5 +1,12 @@
 1.  Basic scaffolding in place:  app.js, landing.ejs, campgrounds.ejs.  No real functionality yet.
 
 2.  changes
-    - Create header and footer partials.
-    - Include bootstrap.  Some styling.
+    1. Create header and footer partials.
+    2. Include bootstrap.  Some styling.
+
+3.  Added creating new campground functionality
+    1. setup new campground POST route
+    2. add in body-parser 
+    3. setup route to show form
+    4. add basic form
+
